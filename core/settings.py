50b0ee28314637759945f9253170e40503e87c7b
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-01-03 22:08:47 +0100

    Heroku files part 4

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
