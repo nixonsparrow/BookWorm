0af6ca91470a6687b1b79ec3474688bcdc08fc49
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-01-04 17:07:46 +0100

    Dynamic search lacks only date published criterium

--- books/admin.py
+++ books/admin.py
@@ -3,7 +3,7 @@ from books.models import Book
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['title', 'author', 'language', 'pub_date', 'isbn', 'page_count', 'id']
 
 
 admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Book(models.Model):
+    class Meta:
+        ordering = ['title']
+
     title = models.CharField('Title', max_length=100, default='', null=False, blank=False)
     author = models.CharField('Author', max_length=100, default='', null=True, blank=True)
     cover_link = models.CharField('Link to the cover', default='', max_length=150, null=True, blank=True)
@@ -13,3 +15,6 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13, default='', null=True, blank=True)
     isbn_10 = models.CharField('ISBN 10', max_length=10, default='', null=True, blank=True)
     page_count = models.IntegerField('Pages', default=None, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}{" (" + str(self.author) + ")" if self.author else ""}'
