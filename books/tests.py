399c8349982bebdbeeaff024a54f0f4287ab5da3
Nikodem Wróbel <nixonsparrow@gmail.com>
2022-01-11 01:33:10 +0100

    Include template and standard response tests

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 from books.models import Book
 from books.languages import (
     get_language_tags, get_language_names, is_language_name, is_language_tag,
@@ -50,4 +51,63 @@ class LanguageMethodsTestCase(TestCase):
         self.assertIn(('en', 'English'), get_languages_from_languages_txt())
         self.assertIn(('pl', 'Polish'), get_languages_from_languages_txt())
         self.assertIn(('xx', 'Not specified'), get_languages_from_languages_txt())
+
+
+class TemplatesUsedTestCase(TestCase):
+    def setUp(self):
+        self.book = Book.objects.create(title='There is no spoon!', author='Neo, Mr. Anderson')
+
+    def test_book_list_uses_proper_templates(self):
+        response = self.client.get(reverse('book-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/book_list.html')
+        self.assertTemplateUsed(response, 'books/book_segment.html')
+        self.assertTemplateUsed(response, 'books/search_results.html')
+        self.assertTemplateUsed(response, 'books/base.html')
+
+    def test_book_list_search_uses_proper_templates(self):
+        response = self.client.get(reverse('search'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/search_results.html')
+        self.assertTemplateUsed(response, 'books/book_segment.html')
+
+        self.assertTemplateNotUsed(response, 'books/base.html')
+
+    def test_book_create_uses_proper_templates(self):
+        response = self.client.get(reverse('book-create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/book_form.html')
+        self.assertTemplateUsed(response, 'books/base.html')
+
+    def test_book_update_uses_proper_templates(self):
+        response = self.client.get(reverse('book-update', kwargs={'pk': self.book.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/book_form.html')
+        self.assertTemplateUsed(response, 'books/base.html')
+
+    def test_book_delete_uses_proper_templates(self):
+        response = self.client.get(reverse('book-delete', kwargs={'pk': self.book.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/book_confirm_delete.html')
+        self.assertTemplateUsed(response, 'books/base.html')
+
+    def test_book_import_from_google_uses_proper_templates(self):
+        response = self.client.get(reverse('book-import-google'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/import_from_google.html')
+        self.assertTemplateUsed(response, 'books/import_from_google_results.html')
+        self.assertTemplateUsed(response, 'books/base.html')
+
+    def test_book_import_from_google_results_uses_proper_templates(self):
+        response = self.client.get(reverse('google-search'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/import_from_google_results.html')
+
+        self.assertTemplateNotUsed(response, 'books/base.html')
+
+    def test_book_api_uses_no_templates(self):
+        response = self.client.get(reverse('book-api'))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateNotUsed(response, 'books/base.html')
 
